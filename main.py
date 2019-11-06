e43b7439f65f532b5dfc5a85cca4f64890cd21f0
igorcp <igorcp713@gmail.com>
2019-11-05 21:11:07 -0300

    Com comentario

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-a=2
-b=5
+a=2 # Variavel "a" recebe o valor (2)
+b=5 # Variavel "b" recebe o valor (5)
 
-soma = a + b
+soma = a + b     # soma das variaveis
 
-if(soma > a):
-    print ("Soma maior que a")
-elif(soma == a):
+if(soma > a):    # se soma for menor que "a" 
+    print ("Soma menor que a")
+elif(soma == a): # ou igual a "a"
     print("Soma igual a a")
-else:
+else:            # ou maior que
     print("Soma maior que a")
